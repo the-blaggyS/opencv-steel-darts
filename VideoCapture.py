dccd54d82479f4134ba61303d6d63c73cb851bea
the.blaggy <the.blaggy@gmail.com>
2021-11-17 11:48:23 +0100

    Removed VideoWriter from VideoCapture.py

--- VideoCapture.py
+++ VideoCapture.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from threading import Thread
 
 import cv2
@@ -12,21 +11,17 @@ class VideoStream:
         self.stream.set(cv2.CAP_PROP_FRAME_HEIGHT, 600)
         self.grabbed, self.frame = self.stream.read()
         self.stopped = False
-        self.vout = cv2.VideoWriter()
 
     def start(self):
+        self.stopped = False
         Thread(target=self.update).start()
-        self.vout.open(f'Videos/match_{datetime.now().strftime("%Y-%m-%d_%H:%M")}.mov',
-                       cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), 30, (800, 600), True)
 
     def update(self):
         while not self.stopped:
             self.grabbed, self.frame = self.stream.read()
-            self.vout.write(self.frame)
 
     def read(self):
         return self.frame
 
     def stop(self):
         self.stopped = True
-        self.vout.release()
